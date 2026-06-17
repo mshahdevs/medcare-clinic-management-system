667539085087b0448f3453f61bd8cd6682a5efdb
mshahdevs <mshahaurangzeb45@gmail.com>
2026-06-17 14:32:42 +0500

    added server js file

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+
+import connectDB from './config/db.js';
+
+import authRoutes from './routes/authRoutes.js';
+import adminRoutes from './routes/adminRoutes.js';
+import doctorRoutes from './routes/doctorRoutes.js';
+
+// import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+await connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'Medcare Clinic Management API is running',
+    data: {},
+  });
+});
+
+app.use('/api/auth', authRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/doctors', doctorRoutes);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
