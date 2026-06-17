f3c844bb77bfa57c5530a14ebc499281d2aed335
ummer jadoon <ummerjadoon80@gmail.com>
2026-06-17 14:35:25 +0500

    Refactored to User and Appointment models

--- PatientProfile.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const patientProfileSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  medicalHistory: String,
-  contact: String
-});
-
-module.exports = mongoose.model('PatientProfile', patientProfileSchema);
