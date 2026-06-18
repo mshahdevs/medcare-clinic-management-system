89bcf0a45cee8f1ca4864eef868f18aa232f2a40
mshahdevs <mshahaurangzeb45@gmail.com>
2026-06-18 17:16:33 +0500

    added routes for all appointments , doctorAppointments and updateAppointmentStatus

--- backend/routes/appointmentRoutes.js
+++ backend/routes/appointmentRoutes.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import {
   bookAppointment,
+  getAllAppointments,
+  getDoctorAppointments,
   getMyAppointments,
+  updateAppointmentStatus,
 } from '../controller/appointmentController.js';
 import { protect } from '../middleware/authMiddleware.js';
+import { authorize } from '../middleware/roleMiddleware.js';
 
 const router = express.Router();
 
-router.post('/', protect, bookAppointment);
-router.get('/my', protect, getMyAppointments);
+router.post('/', protect, authorize('patient'), bookAppointment);
+router.get('/my', protect, authorize('patient'), getMyAppointments);
+router.get('/', protect, authorize('admin'), getAllAppointments);
+router.get('/doctor/my', protect, authorize('doctor'), getDoctorAppointments);
+
+router.put('/:id/status', protect, authorize('admin'), updateAppointmentStatus);
 
 export default router;
