b98109fc2865c2c74347f1bcd77012dc191488cb
mshahdevs <mshahaurangzeb45@gmail.com>
2026-06-18 17:19:27 +0500

    added role

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { createDoctor } from '../controller/adminController.js';
+import { createDoctor, getDashboard } from '../controller/adminController.js';
+import { authorize } from '../middleware/roleMiddleware.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/doctors', createDoctor);
-
+router.post('/doctors', protect, authorize('admin'), createDoctor);
+router.get('/dashboard', protect, authorize('admin'), getDashboard);
 export default router;
