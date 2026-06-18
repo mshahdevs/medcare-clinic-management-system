bcde1c4d07ef65b8a5b48c880ec19d194bf34e0a
mshahdevs <mshahaurangzeb45@gmail.com>
2026-06-18 17:19:04 +0500

    added routes for getDoctorDashboard

--- backend/routes/doctorRoutes.js
+++ backend/routes/doctorRoutes.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import {
   getAllDoctors,
+  getDoctorDashboard,
   getSingleDoctor,
 } from '../controller/doctorController.js';
+import { protect } from '../middleware/authMiddleware.js';
+import { authorize } from '../middleware/roleMiddleware.js';
 
 const router = express.Router();
 
 router.get('/', getAllDoctors);
 router.get('/:id', getSingleDoctor);
+router.get(
+  '/doctor/dashboard',
+  protect,
+  authorize('doctor'),
+  getDoctorDashboard,
+);
 
 export default router;
