3a6f59f6b341bf1c732a759e00c3584acc52ce56
mshahdevs <mshahaurangzeb45@gmail.com>
2026-06-21 16:29:29 +0500

    added frontend url for cors

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,17 @@ const PORT = process.env.PORT || 5000;
 
 await connectDB();
 
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://online-doctor-appointment-system-five.vercel.app/',
+    ],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
