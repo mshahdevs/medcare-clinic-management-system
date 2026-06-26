baed9ed223c692652bb8a2ae47268eb99e0fced5
mshahdevs <mshahaurangzeb45@gmail.com>
2026-06-26 17:47:17 +0500

    added notification model

---
+++ backend/models/Notification.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const notificationSchema = new mongoose.Schema(
+  {
+    patient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model('Notification', notificationSchema);
